8055570f405d27ac35062de288d3d8722bd8f52b
rusick1112 <145065735+rusick1112@users.noreply.github.com>
2025-05-07 18:15:42 +0300

    последний шаг вроде правильно, добавил в main.js

--- kids-clothing-shop/src/main.js
+++ kids-clothing-shop/src/main.js
@@ -3,7 +3,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-import './assets/css/main.scss'
+import './assets/css/main.css'
 
 const app = createApp(App)
 
