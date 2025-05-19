a06ddcff7fa702e22ef573305df95a3d46e4a058
rusick1112 <145065735+rusick1112@users.noreply.github.com>
2025-05-19 12:08:54 +0300

    удаление tailwind

--- kids-clothing-shop/src/main.js
+++ kids-clothing-shop/src/main.js
@@ -3,7 +3,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-import './assets/css/main.css'
 
 const app = createApp(App)
 
